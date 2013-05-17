ce1a9d3249e431c470906f0158e0672f92f62792
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2013-05-17 13:44:19 +0200

    added session handling

--- routes/ui.js
+++ routes/ui.js
@@ -9,7 +9,16 @@ var config = require('../config/server').config;
 
 var startUI = function(req, res, next) {
 	var loggedIn = false;
-	if (typeof req.session!=='undefined'){
+	if (typeof req.query!=='undefined'){
+		if (req.query.logout==='1'){
+			req.session.destroy();
+			res.clearCookie(config.auth_cookie_name, {
+				path : '/'
+			});
+			res.redirect('/');
+		}
+	}
+	if ((typeof req.session!=='undefined')&& (req.session!==null)){
 		if (typeof req.session.user!=='undefined'){
 			loggedIn = req.session.user.loggedIn;
 		}
